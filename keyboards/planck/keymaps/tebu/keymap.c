83edad2b62052b5ff0fb1543cb192bab4dff71aa
Jaeyoung <jaeyoung@Ebranurs-MacBook-Air.local>
2025-04-24 13:10:43 +0900

    .

--- keyboards/planck/keymaps/tebu/keymap.c
+++ keyboards/planck/keymaps/tebu/keymap.c
@@ -1,4 +1,5 @@
 #include QMK_KEYBOARD_H
+#include "keymap_japanese.h"
 #ifdef AUDIO_ENABLE
 #include "muse.h"
 #endif
@@ -7,6 +8,7 @@
 enum planck_keycodes {
   RGB_SLD = EZ_SAFE_RANGE,
   MAC_LOCK,
+  JP_TOG,
 };
 
 
@@ -23,7 +25,7 @@ enum planck_layers {
   _ADJUST,
   _LAYER5,
   _LAYER6,
-  _LAYER7,
+  _SYMBOLS,
   _LAYER8,
 };
 
@@ -35,17 +37,17 @@ enum planck_layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_LAYER0] = LAYOUT_planck_grid(
-    KC_Q,         KC_W,  KC_E,           KC_R,                KC_T,            KC_0,          KC_3,     KC_Y,        KC_U,              KC_I,           KC_O,           KC_P,
-    KC_A,         KC_S,  LT(6,KC_D),     LT(7,KC_F),          KC_G,            KC_1,          KC_4,     KC_H,        GUI_T(KC_J),       CTL_T(KC_K),    ALT_T(KC_L),    KC_SCLN,
-    SFT_T(KC_Z),  KC_X,  KC_C,           ALT_T(KC_V),         KC_B,            KC_2,          KC_5,     KC_N,        KC_M,              TG(5),          KC_DOT,         KC_RIGHT_SHIFT, 
-    TG(3),        TG(8), LT(2,KC_ESCAPE),THUMB0,              THUMB1,          KC_9,          KC_NO,    RAISE,       KC_ENTER,          C(KC_SPACE),    KC_CAPS,        TG(1)
+    KC_Q,         KC_W,       KC_E,           KC_R,       KC_T,         KC_0,      JP_TOG,   KC_Y,        KC_U,              KC_I,           KC_O,           KC_P,
+    KC_A,         KC_S,       LT(6,KC_D),     LT(7,KC_F), KC_G,         KC_1,      KC_4,     KC_H,        GUI_T(KC_J),       CTL_T(KC_K),    ALT_T(KC_L),    KC_SCLN,
+    SFT_T(KC_Z),  KC_X,       KC_C,           KC_V,       KC_B,         KC_2,      KC_5,     KC_N,        KC_M,              KC_COMMA,       KC_DOT,         KC_RIGHT_SHIFT, 
+    KC_TAB,       TG(8),      ALT_T(KC_ESCAPE),THUMB0,    THUMB1,       TG(3),     KC_NO,    RAISE,       LT(2,KC_ENTER),    TG(5),          KC_CAPS,        TG(1)
   ),
 
   [_BASE] = LAYOUT_planck_grid(
     KC_Q,           KC_Z,           KC_F,           KC_P,           KC_V,           _______,  _______, KC_J,           KC_L,           KC_U,           KC_Y,           KC_X,
     KC_A,           KC_R,           LT(6,KC_S),     LT(7,KC_T),     KC_G,           _______,  _______, KC_K,           GUI_T(KC_N),    CTL_T(KC_E),    ALT_T(KC_I),    KC_O,
-    KC_LSFT,        KC_W,           KC_C,           ALT_T(KC_D),    KC_B,           _______,  _______, KC_M,           KC_H,           TG(5),          KC_DOT,         KC_RSFT,
-    _______,        _______,        _______,        _______,        _______,        _______,  KC_NO,   RAISE,          LT(_LOWER, KC_ENTER),_______,   _______,        _______
+    KC_LSFT,        KC_W,           KC_C,           KC_D,           KC_B,           _______,  _______, KC_M,           KC_H,           KC_COMMA,       KC_DOT,         KC_RSFT,
+    _______,        _______,        _______,        _______,        _______,        _______,  KC_NO,   RAISE,          LT(2,KC_ENTER), _______,   _______,        _______
   ),
 
   [_LOWER] = LAYOUT_planck_grid(
@@ -56,14 +58,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_RAISE] = LAYOUT_planck_grid(
-    KC_LBRC,        KC_RBRC,        KC_LPRN,        KC_RPRN,        KC_DQUO,        _______, _______, KC_SCLN,        KC_7,           KC_8,           KC_9,           KC_BSLS,        
-    KC_QUOTE,       KC_DOT,         KC_COMMA,       KC_0,           KC_LCBR,        _______, _______, KC_RCBR,        KC_1,           KC_2,           KC_3,           KC_COLN,        
-    KC_LSFT,        KC_UNDS,        KC_MINUS,       KC_EQUAL,       KC_GRAVE,       _______, _______, KC_SLASH,       KC_4,           KC_5,           KC_6,           KC_RSFT, 
-    _______, _______, KC_TAB,         _______, _______, _______, KC_NO,          _______, _______, _______, _______, TO(1)
+    KC_LBRC,        KC_RBRC,        KC_LPRN,        KC_RPRN,        KC_DQUO,        KC_INT3,  _______, KC_SCLN,        KC_7,           KC_8,           KC_9,           KC_BSLS,
+    KC_QUOT,        KC_DOT,         KC_COMMA,       KC_0,           KC_LCBR,        KC_EQL,  _______, KC_RCBR,        KC_1,           KC_2,           KC_3,           KC_COLN,
+    KC_LSFT,        KC_UNDS,        KC_MINUS,       KC_EQUAL,       KC_GRAVE,       KC_INT1, _______, KC_SLASH,       KC_4,           KC_5,           KC_6,           KC_RSFT,
+    _______,        _______,        KC_TAB,         _______,        _______,        _______, KC_NO,   _______,        _______,        KC_0,           _______,        TO(1)
   ),
 
   [_ADJUST] = LAYOUT_planck_grid(
-    RGB_TOG,        RGB_MODE_FORWARD,RGB_VAD,        RGB_VAI,        _______, _______, _______, KC_AUDIO_MUTE,  KC_AUDIO_VOL_DOWN,KC_AUDIO_VOL_UP,LED_LEVEL,      MAC_LOCK,       
+    RGB_TOG, RGB_MODE_FORWARD,RGB_VAD,        RGB_VAI,        _______, _______, _______, KC_AUDIO_MUTE,  KC_AUDIO_VOL_DOWN,KC_AUDIO_VOL_UP,LED_LEVEL,      MAC_LOCK,       
     _______, _______, _______, _______, _______, _______, _______, KC_MEDIA_PREV_TRACK,KC_MEDIA_PLAY_PAUSE,KC_MEDIA_NEXT_TRACK,KC_BRIGHTNESS_DOWN,KC_BRIGHTNESS_UP,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_SYSTEM_SLEEP,
     _______, _______, _______, _______, _______, _______, KC_NO,          _______, _______, _______, _______, _______
@@ -73,8 +75,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_LAYER5] = LAYOUT_planck_grid(
     KC_NO,          KC_NO,          KC_MS_UP,       KC_NO,          KC_NO,          _______, _______, KC_NO,          KC_NO,          KC_MS_WH_UP,    KC_NO,          KC_NO,          
     KC_NO,          KC_MS_LEFT,     KC_MS_DOWN,     KC_MS_RIGHT,    KC_NO,          _______, _______, KC_NO,          KC_MS_BTN1,     KC_MS_WH_DOWN,  KC_MS_BTN2,     KC_NO,          
-    KC_LEFT_SHIFT,  KC_LEFT_ALT,    KC_NO,          KC_NO,          KC_NO,          _______, _______, KC_NO,          KC_MS_WH_LEFT,  _______, KC_MS_WH_RIGHT, KC_NO,          
-    _______, _______, _______, KC_LEFT_CTRL,   KC_LEFT_ALT,    _______, KC_NO,          KC_MS_ACCEL2,   _______, _______, _______, TO(1)
+    KC_LSFT,        KC_LALT,        KC_NO,          KC_NO,          KC_NO,          _______, _______, KC_NO,          KC_MS_WH_LEFT,  _______,        KC_MS_WH_RIGHT, KC_NO,          
+    _______,        _______,        _______,        _______,        KC_LCTL,        _______, KC_NO,   KC_MS_ACCEL2,   _______,        _______,        _______,        TO(1)
   ),
 
   // ARROWS
@@ -82,23 +84,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,          KC_NO,          KC_NO,     KC_NO,    KC_NO,          _______, _______, KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          
     KC_LGUI,        KC_LALT,        _______,   KC_LSFT,  KC_NO,          _______, _______, KC_NO,          KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       
     KC_NO,          KC_NO,          KC_NO,     KC_NO,    KC_NO,          _______, _______, KC_NO,          KC_HOME,        KC_PGDN,        KC_PAGE_UP,     KC_END,         
-    TO(0),          _______,        _______,   _______,  KC_LCTL,  _______, KC_NO,          _______, _______, _______, _______, TO(1)
+    TO(0),          _______,        KC_LALT,   KC_LGUI,  KC_LCTL,        _______, KC_NO,   _______,        _______,        _______,        _______,        TO(1)
   ),
 
   // SYMBOLS
-  [_LAYER7] = LAYOUT_planck_grid(
-    KC_LBRC,        KC_RBRC,        KC_LPRN,        KC_RPRN,        KC_QUOTE,       _______, _______, KC_SCLN,        KC_AMPR,        KC_ASTR,        KC_NO,          KC_PIPE,        
-    KC_DQUO,        KC_DOT,         KC_COMMA,       _______, _______, _______, _______, KC_RCBR,        KC_EXLM,        KC_AT,          KC_HASH,        KC_COLN,        
-    _______, KC_UNDS,        KC_MINUS,       _______, _______, _______, _______, KC_QUES,        KC_DLR,         KC_PERC,        KC_CIRC,        _______, 
-    _______, _______, _______, _______, _______, _______, KC_NO,          _______, _______, _______, _______, _______
+  [_SYMBOLS] = LAYOUT_planck_grid(
+    KC_LBRC,        KC_RBRC,        KC_LPRN,        KC_RPRN, KC_QUOTE,  _______, _______, KC_SCLN,        KC_AMPR,        KC_ASTR,        KC_LPRN,        KC_PIPE,        
+    KC_DQUO,        KC_DOT,         KC_COMMA,       _______, KC_LCBR,   _______, _______, KC_RCBR,        KC_EXLM,        KC_AT,          KC_HASH,        KC_COLN,        
+    KC_LSFT,        KC_UNDS,        KC_MINUS,       KC_EQL,  KC_TILDE,  _______, _______, KC_QUES,        KC_DLR,         KC_PERC,        KC_CIRC,        KC_RSFT,
+    _______,        _______,        _______,        _______, _______,   _______, KC_NO,   _______,        _______,        _______,        _______,        _______
   ),
 
   // WITHMOUSE
   [_LAYER8] = LAYOUT_planck_grid(
     KC_F1,          TD(DANCE_0),    TD(DANCE_1),    KC_F4,          KC_F5,          KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,         KC_F12,         
     KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           _______,        _______,        _______,        KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       
-    SFT_T(KC_6),    KC_7,           KC_8,           ALT_T(KC_9),    KC_0,           _______,        _______,        _______,        KC_HOME,        KC_PGDN,        KC_PGUP,        KC_END,         
-    _______,        _______,        _______,        KC_LCTL,        KC_LGUI,        _______,        KC_NO,          _______,        _______,        _______,        _______,        _______         
+    SFT_T(KC_6),    ALT_T(KC_7),    KC_8,           KC_9,           KC_0,           _______,        _______,        _______,        KC_HOME,        KC_PGDN,        KC_PGUP,        KC_END,         
+    _______,        _______,        KC_ESCAPE,      THUMB0,         THUMB1,         _______,        KC_NO,          _______,        _______,        _______,        _______,        _______         
   ),
 
 };
@@ -106,7 +108,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 const uint16_t PROGMEM tab_combo[] = {KC_F, LT(7,KC_T), COMBO_END};
 const uint16_t PROGMEM left_tab_combo[] = {KC_C, THUMB0, COMBO_END};
-const uint16_t PROGMEM right_tab_combo[] = {MT(MOD_LALT,KC_D), THUMB0, COMBO_END};
+const uint16_t PROGMEM right_tab_combo[] = {KC_D, THUMB0, COMBO_END};
 const uint16_t PROGMEM left_workspace_combo[] = {LT(6,KC_S), MT(MOD_LALT,KC_D), THUMB0, COMBO_END};
 const uint16_t PROGMEM right_workspace_combo[] = {LT(6,KC_S), MT(MOD_LALT,KC_D), THUMB1, COMBO_END};
 const uint16_t PROGMEM mission_control_combo[] = {KC_F, LT(7,KC_T), THUMB0, COMBO_END};
@@ -185,7 +187,49 @@ bool rgb_matrix_indicators_user(void) {
   return true;
 }
 
+static bool japanese_mode = false;
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  /* --- language toggle key --- */
+  if (keycode == JP_TOG && record->event.pressed) {
+      japanese_mode = !japanese_mode;   /* flip EN/JP flag */
+      return false;                     /* eat the key */
+  }
+
+  /* --- dynamic JP remapping for _RAISE / _SYMBOLS --- */
+  if (japanese_mode && (layer_state_is(_RAISE) || layer_state_is(_SYMBOLS))) {
+      uint16_t jp_code = 0;
+      switch (keycode) {
+          case KC_UNDS:  jp_code = JP_UNDS;  break;
+          case KC_EQUAL: jp_code = JP_EQL;   break;
+          case KC_LBRC:  jp_code = JP_LBRC;  break;
+          case KC_RBRC:  jp_code = JP_RBRC;  break;
+          case KC_BSLS:  jp_code = JP_YEN;   break;
+          case KC_LCBR:  jp_code = JP_LCBR;  break;
+          case KC_RCBR:  jp_code = JP_RCBR;  break;
+          case KC_LPRN:  jp_code = JP_LPRN;  break;
+          case KC_RPRN:  jp_code = JP_RPRN;  break;
+          case KC_SCLN:  jp_code = JP_SCLN;  break;
+          case KC_COLN:  jp_code = JP_COLN;  break;
+          case KC_QUOT:  jp_code = JP_QUOT;  break;
+          case KC_DQUO:  jp_code = JP_DQUO;  break;
+
+          case KC_AT:    jp_code = JP_AT;  break;
+          case KC_CIRC:  jp_code = JP_CIRC;  break;
+          case KC_AMPR:  jp_code = JP_AMPR;  break;
+          case KC_ASTR:  jp_code = JP_ASTR;  break;
+      }
+
+      if (jp_code) {                     /* remap if matched */
+          if (record->event.pressed) {
+              register_code16(jp_code);
+          } else {
+              unregister_code16(jp_code);
+          }
+          return false;                  /* stop further handling */
+      }
+  }
+
   switch (keycode) {
     case MAC_LOCK:
       HCS(0x19E);
